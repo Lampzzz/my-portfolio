4d4a928dd02ffa0f2812c48a607d80aaadf615a3
Lampz <lampazaj@gmail.com>
2025-06-23 09:36:45 +0800

    initial commit

--- src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="bg-white h-screen flex items-center justify-center w-full">
-      <div className="">{children}</div>
-    </main>
-  );
-};
